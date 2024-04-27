4835ec6ce068697db748c222f23ea6b481cb6841
Nats742 <nathan.sidharta@binus.ac.id>
2024-04-27 18:17:44 +0700

    Update back-end Petterinary
    add backend structure

---
+++ config.js
@@ -0,0 +1,14 @@
+const firebase = require("firebase");
+const firebaseConfig = {
+  apiKey: "AIzaSyCJpuZdn5dXqbUYdybDLsbGEv9yptdQ4kU",
+  authDomain: "petterinary-d36f0.firebaseapp.com",
+  projectId: "petterinary-d36f0",
+  storageBucket: "petterinary-d36f0.appspot.com",
+  messagingSenderId: "821654324244",
+  appId: "1:821654324244:web:a131b32e3d5da3d7aad301",
+  measurementId: "G-WJ98JRG696"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const User = db.collection("Users");
+module.exports = User;
