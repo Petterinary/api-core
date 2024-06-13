30d475ef53d7e0975f86333f62418c9dd20ec824
Daevan <115785730+DaevanM@users.noreply.github.com>
2024-06-13 22:05:48 +0700

    update registration form

--- models/serviceRegistrationForm.js
+++ models/serviceRegistrationForm.js
@@ -1,85 +1,112 @@
+const { FieldValue } = require("firebase-admin/firestore");
 const db = require("../firebaseAdmin");
 
 const ServiceRegistrationFormRead = {
   getAllServiceRegistrationForms: async () => {
     try {
-      const snapshot = await db.collection("ServiceRegistrationForms").orderBy("serviceRegistrationFormId", "asc").get();
-      const list = await Promise.all(snapshot.docs.map(async (doc) => {
-        const data = doc.data();
+      const snapshot = await db
+        .collection("ServiceRegistrationForms")
+        .orderBy("serviceRegistrationFormId", "asc")
+        .get();
+      const list = await Promise.all(
+        snapshot.docs.map(async (doc) => {
+          const data = doc.data();
 
-        let doctorName = "";
-        let userName = "";
-        let address = "";
-        let lat = "";
-        let lng = "";
+          let doctorName = "";
+          let userName = "";
+          let address = "";
+          let lat = "";
+          let lng = "";
 
-        // Fetch user details
-        if (data.userId) {
-          try {
-            const userQuery = await db.collection("Users").where("userId", "==", data.userId).get();
-            if (!userQuery.empty) {
-              const userData = userQuery.docs[0].data();
-              userName = userData.name;
-              if (data.visitType === 1) {
-                address = userData.address;
-                lat = userData.lat;
-                lng = userData.lng;
+          // Fetch user details
+          if (data.userId) {
+            try {
+              const userQuery = await db
+                .collection("Users")
+                .where("userId", "==", data.userId)
+                .get();
+              if (!userQuery.empty) {
+                const userData = userQuery.docs[0].data();
+                userName = userData.name;
+                if (data.visitType === 1) {
+                  address = userData.address;
+                  lat = userData.lat;
+                  lng = userData.lng;
+                }
+              } else {
+                console.log(`User with ID ${data.userId} not found.`);
               }
-            } else {
-              console.log(`User with ID ${data.userId} not found.`);
+            } catch (error) {
+              console.error(
+                `Error fetching user data for ID ${data.userId}: ${error.message}`
+              );
             }
-          } catch (error) {
-            console.error(`Error fetching user data for ID ${data.userId}: ${error.message}`);
           }
-        }
 
-        // Fetch doctor details
-        if (data.doctorId) {
-          try {
-            const doctorQuery = await db.collection("Doctors").where("doctorId", "==", data.doctorId).get();
-            if (!doctorQuery.empty) {
-              const doctorData = doctorQuery.docs[0].data();
-              doctorName = doctorData.name;
-              if (data.visitType === 2) {
-                address = doctorData.address;
-                lat = doctorData.lat;
-                lng = doctorData.lng;
+          // Fetch doctor details
+          if (data.doctorId) {
+            try {
+              const doctorQuery = await db
+                .collection("Doctors")
+                .where("doctorId", "==", data.doctorId)
+                .get();
+              if (!doctorQuery.empty) {
+                const doctorData = doctorQuery.docs[0].data();
+                doctorName = doctorData.name;
+                if (data.visitType === 2) {
+                  address = doctorData.address;
+                  lat = doctorData.lat;
+                  lng = doctorData.lng;
+                }
+              } else {
+                console.log(`Doctor with ID ${data.doctorId} not found.`);
               }
-            } else {
-              console.log(`Doctor with ID ${data.doctorId} not found.`);
+            } catch (error) {
+              console.error(
+                `Error fetching doctor data for ID ${data.doctorId}: ${error.message}`
+              );
             }
-          } catch (error) {
-            console.error(`Error fetching doctor data for ID ${data.doctorId}: ${error.message}`);
           }
-        }
 
-        return {
-          serviceRegistrationFormId: data.serviceRegistrationFormId || null,
-          registrationDate: data.registrationDate ? data.registrationDate.toDate() : null,
-          address,
-          lat,
-          lng,
-          petName: data.petName || "",
-          petType: data.petType || "",
-          complaint: data.complaint || "",
-          visitType: data.visitType || 0,
-          createdAt: data.createdAt ? data.createdAt.toDate() : null,
-          updatedAt: data.updatedAt ? data.updatedAt.toDate() : null,
-          doctorId: data.doctorId || "",
-          doctorName,
-          userId: data.userId || "",
-          userName,
-        };
-      }));
+          return {
+            serviceRegistrationFormId: data.serviceRegistrationFormId || null,
+            registrationDate: data.registrationDate
+              ? data.registrationDate.toDate()
+              : null,
+            address,
+            lat,
+            lng,
+            petName: data.petName || "",
+            petType: data.petType || "",
+            complaint: data.complaint || "",
+            visitType: data.visitType || 0,
+            createdAt: data.createdAt ? data.createdAt.toDate() : null,
+            updatedAt: data.updatedAt ? data.updatedAt.toDate() : null,
+            doctorId: data.doctorId || "",
+            doctorName,
+            userId: data.userId || "",
+            userName,
+          };
+        })
+      );
       return list;
     } catch (error) {
-      throw new Error("Failed to fetch service registration forms: " + error.message);
+      throw new Error(
+        "Failed to fetch service registration forms: " + error.message
+      );
     }
   },
 
   getServiceRegistrationFormById: async (serviceRegistrationFormId) => {
     try {
-      const querySnapshot = await db.collection("ServiceRegistrationForms").where("serviceRegistrationFormId", "==", parseInt(serviceRegistrationFormId)).get();
+      const querySnapshot = await db
+        .collection("ServiceRegistrationForms")
+        .where(
+          "serviceRegistrationFormId",
+          "==",
+          parseInt(serviceRegistrationFormId)
+        )
+        .get();
       if (querySnapshot.empty) {
         throw new Error("Service registration form not found");
       }
@@ -95,7 +122,10 @@ const ServiceRegistrationFormRead = {
       // Fetch user details
       if (data.userId) {
         try {
-          const userQuery = await db.collection("Users").where("userId", "==", data.userId).get();
+          const userQuery = await db
+            .collection("Users")
+            .where("userId", "==", data.userId)
+            .get();
           if (!userQuery.empty) {
             const userData = userQuery.docs[0].data();
             userName = userData.name;
@@ -108,14 +138,19 @@ const ServiceRegistrationFormRead = {
             console.log(`User with ID ${data.userId} not found.`);
           }
         } catch (error) {
-          console.error(`Error fetching user data for ID ${data.userId}: ${error.message}`);
+          console.error(
+            `Error fetching user data for ID ${data.userId}: ${error.message}`
+          );
         }
       }
 
       // Fetch doctor details
       if (data.doctorId) {
         try {
-          const doctorQuery = await db.collection("Doctors").where("doctorId", "==", data.doctorId).get();
+          const doctorQuery = await db
+            .collection("Doctors")
+            .where("doctorId", "==", data.doctorId)
+            .get();
           if (!doctorQuery.empty) {
             const doctorData = doctorQuery.docs[0].data();
             doctorName = doctorData.name;
@@ -128,13 +163,17 @@ const ServiceRegistrationFormRead = {
             console.log(`Doctor with ID ${data.doctorId} not found.`);
           }
         } catch (error) {
-          console.error(`Error fetching doctor data for ID ${data.doctorId}: ${error.message}`);
+          console.error(
+            `Error fetching doctor data for ID ${data.doctorId}: ${error.message}`
+          );
         }
       }
 
       return {
         serviceRegistrationFormId: data.serviceRegistrationFormId || null,
-        registrationDate: data.registrationDate ? data.registrationDate.toDate() : null,
+        registrationDate: data.registrationDate
+          ? data.registrationDate.toDate()
+          : null,
         address,
         lat,
         lng,
@@ -150,15 +189,30 @@ const ServiceRegistrationFormRead = {
         userName,
       };
     } catch (error) {
-      throw new Error("Failed to fetch service registration form: " + error.message);
+      throw new Error(
+        "Failed to fetch service registration form: " + error.message
+      );
     }
   },
 };
 
 const ServiceRegistrationFormWrite = {
-  createServiceRegistrationForm: async ({ registrationDate, address, petName, petType, complaint, visitType, doctorId, userId, lat, lng }) => {
+  createServiceRegistrationForm: async ({
+    registrationDate,
+    address,
+    petName,
+    petType,
+    complaint,
+    visitType,
+    doctorId,
+    userId,
+    lat,
+    lng,
+  }) => {
     try {
-      const counterRef = db.collection("ServiceRegistrationFormCounter").doc("serviceRegistrationFormCounter");
+      const counterRef = db
+        .collection("ServiceRegistrationFormCounter")
+        .doc("serviceRegistrationFormCounter");
       const counterDoc = await counterRef.get();
 
       let newCount;
@@ -171,7 +225,9 @@ const ServiceRegistrationFormWrite = {
         await counterRef.update({ count: newCount });
       }
 
-      const newServiceRegistrationFormRef = db.collection("ServiceRegistrationForms").doc();
+      const newServiceRegistrationFormRef = db
+        .collection("ServiceRegistrationForms")
+        .doc();
       const newServiceRegistrationFormData = {
         serviceRegistrationFormId: newCount,
         registrationDate: new Date(registrationDate),
@@ -194,16 +250,28 @@ const ServiceRegistrationFormWrite = {
         serviceRegistrationFormId: newCount,
         ...newServiceRegistrationFormData,
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       };
     } catch (error) {
-      throw new Error("Failed to create service registration form: " + error.message);
+      throw new Error(
+        "Failed to create service registration form: " + error.message
+      );
     }
   },
 
-  updateServiceRegistrationFormById: async (serviceRegistrationFormId, newData) => {
+  updateServiceRegistrationFormById: async (
+    serviceRegistrationFormId,
+    newData
+  ) => {
     try {
-      const querySnapshot = await db.collection("ServiceRegistrationForms").where("serviceRegistrationFormId", "==", parseInt(serviceRegistrationFormId)).get();
+      const querySnapshot = await db
+        .collection("ServiceRegistrationForms")
+        .where(
+          "serviceRegistrationFormId",
+          "==",
+          parseInt(serviceRegistrationFormId)
+        )
+        .get();
       if (querySnapshot.empty) {
         throw new Error("Service registration form not found");
       }
@@ -211,16 +279,28 @@ const ServiceRegistrationFormWrite = {
 
       newData.updatedAt = FieldValue.serverTimestamp();
 
-      await db.collection("ServiceRegistrationForms").doc(doc.id).update(newData);
+      await db
+        .collection("ServiceRegistrationForms")
+        .doc(doc.id)
+        .update(newData);
       return { msg: "Service registration form updated" };
     } catch (error) {
-      throw new Error("Failed to update service registration form: " + error.message);
+      throw new Error(
+        "Failed to update service registration form: " + error.message
+      );
     }
   },
 
   deleteServiceRegistrationFormById: async (serviceRegistrationFormId) => {
     try {
-      const querySnapshot = await db.collection("ServiceRegistrationForms").where("serviceRegistrationFormId", "==", parseInt(serviceRegistrationFormId)).get();
+      const querySnapshot = await db
+        .collection("ServiceRegistrationForms")
+        .where(
+          "serviceRegistrationFormId",
+          "==",
+          parseInt(serviceRegistrationFormId)
+        )
+        .get();
       if (querySnapshot.empty) {
         throw new Error("Service registration form not found");
       }
@@ -228,7 +308,9 @@ const ServiceRegistrationFormWrite = {
       await db.collection("ServiceRegistrationForms").doc(doc.id).delete();
       return { msg: "Service registration form deleted" };
     } catch (error) {
-      throw new Error("Failed to delete service registration form: " + error.message);
+      throw new Error(
+        "Failed to delete service registration form: " + error.message
+      );
     }
   },
 };
